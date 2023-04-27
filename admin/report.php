4b19cb17dcfbf033cdc0d1ffe857246a353d6f8e
AGETRO <brentagetrophil@gmial.com>
2023-04-27 23:59:08 +0800

    added PDF download for report

--- admin/report.php
+++ admin/report.php
@@ -52,72 +52,72 @@ CONCAT(bi2.lastname, ', ', bi2.firstname) as head
                     <div class="d-flex gap-3">
                         <div class="form-group m-0">
                             <label>
-                            <select id="age-filter" class="form-control">
-                                <option value="">Age</option>
-                                <option value="18-24">18-24</option>
-                                <option value="25-34">25-34</option>
-                                <option value="35-44">35-44</option>
-                                <option value="45-54">45-54</option>
-                                <option value="55-64">55-64</option>
-                                <option value="65+">65+</option>
-                            </select>
+                                <select id="age-filter" class="form-control">
+                                    <option value="">Age</option>
+                                    <option value="18-24">18-24</option>
+                                    <option value="25-34">25-34</option>
+                                    <option value="35-44">35-44</option>
+                                    <option value="45-54">45-54</option>
+                                    <option value="55-64">55-64</option>
+                                    <option value="65+">65+</option>
+                                </select>
                             </label>
                         </div>
                         <div class="form-group m-0 ml-3" style="max-width: 180px;">
                             <label>
-                            <select id="brgy" class="form-control">
-                                <option value="">Select Barangay</option>
-                                <?php if ($brgy->num_rows > 0) {
-                                    // output data of each row
-                                    while($row = $brgy->fetch_assoc()) {?>
-                                        // do something with the data
-                                        <option value="<?php echo $row['name']?>" ><?php echo $row['name']?></option>
-                                    <?php }
-                                } else {
-                                    echo "0 results";
-                                } ?>
-                            </select>
+                                <select id="brgy" class="form-control">
+                                    <option value="">Select Barangay</option>
+                                    <?php if ($brgy->num_rows > 0) {
+                                        // output data of each row
+                                        while($row = $brgy->fetch_assoc()) {?>
+                                            // do something with the data
+                                            <option value="<?php echo $row['name']?>" ><?php echo $row['name']?></option>
+                                        <?php }
+                                    } else {
+                                        echo "0 results";
+                                    } ?>
+                                </select>
                             </label>
                         </div>
                         <div class="form-group m-0 ml-3">
                             <label>
-                            <select id="gender" class="form-control">
-                                <option value="">Select Gender</option>
-                                <option value="M">Male</option>
-                                <option value="F">Female</option>
-                            </select>
+                                <select id="gender" class="form-control">
+                                    <option value="">Select Gender</option>
+                                    <option value="M">Male</option>
+                                    <option value="F">Female</option>
+                                </select>
                             </label>
                         </div>
                         <div class="form-group m-0 ml-3" style="max-width: 250px;">
                             <label>
-                            <select id="center" class="form-control">
-                                <option value="">Select Evacuation Center</option>
-                                <?php if ($result->num_rows > 0) {
-                                    // output data of each row
-                                    while($row = $result->fetch_assoc()) {?>
-                                        // do something with the data
-                                        <option value="<?php echo $row['center'] ?>" ><?php echo $row['center'] ?></option>
-                                    <?php }
-                                } else {
-                                    echo "0 results";
-                                } ?>
-                            </select>
+                                <select id="center" class="form-control">
+                                    <option value="">Select Evacuation Center</option>
+                                    <?php if ($result->num_rows > 0) {
+                                        // output data of each row
+                                        while($row = $result->fetch_assoc()) {?>
+                                            // do something with the data
+                                            <option value="<?php echo $row['center'] ?>" ><?php echo $row['center'] ?></option>
+                                        <?php }
+                                    } else {
+                                        echo "0 results";
+                                    } ?>
+                                </select>
                             </label>
                         </div>
                         <div class="form-group m-0 ml-3">
                             <label>
-                            <select id="calamity" class="form-control">
-                                <option value="">Select Disaster</option>
-                                <?php if ($calamity->num_rows > 0) {
-                                    // output data of each row
-                                    while($row = $calamity->fetch_assoc()) {?>
-                                        // do something with the data
-                                        <option value="<?php echo $row['name'] ?>"><?php echo $row['name'] ?></option>
-                                    <?php }
-                                } else {
-                                    echo "0 results";
-                                } ?>
-                            </select>
+                                <select id="calamity" class="form-control">
+                                    <option value="">Select Disaster</option>
+                                    <?php if ($calamity->num_rows > 0) {
+                                        // output data of each row
+                                        while($row = $calamity->fetch_assoc()) {?>
+                                            // do something with the data
+                                            <option value="<?php echo $row['name'] ?>"><?php echo $row['name'] ?></option>
+                                        <?php }
+                                    } else {
+                                        echo "0 results";
+                                    } ?>
+                                </select>
                             </label>
                         </div>
                         <div class="form-group m-0 ml-3">
@@ -128,6 +128,9 @@ CONCAT(bi2.lastname, ', ', bi2.firstname) as head
                                 <option value="Senior Citizen">Senior Citizen</option>
                                 <option value="Special needs">With Special Needs</option>
                             </select>
+                        </div>
+                        <div class="form-group m-0 ml-3">
+                            <button id="generate-pdf" class="btn btn-outline-dark"><i class="far fa-file-pdf"></i> Generate PDF</button>
                         </div>
                     </div>
                 </div>
@@ -166,26 +169,58 @@ CONCAT(bi2.lastname, ', ', bi2.firstname) as head
         </div>
     </div>
 </section>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.68/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.68/vfs_fonts.js"></script>
 <script>
+    document.getElementById('generate-pdf').addEventListener('click', generatePDF);
+    function generatePDF() {
+        // Get table data
+        const table = document.getElementById('myTable');
+        const data = [];
+        for (let i = 0; i < table.rows[0].cells.length; i++) {
+            data[0] = data[0] || [];
+            data[0].push(table.rows[0].cells[i].textContent);
+        }
+        for (let i = 1; i < table.rows.length; i++) {
+            const row = [];
+            for (let j = 0; j < table.rows[i].cells.length; j++) {
+                row.push(table.rows[i].cells[j].textContent);
+            }
+            data.push(row);
+        }
+        const docDefinition = {
+            content: [
+                { text: 'Evacuation Report', style: 'header' },
+                { text: '\n' },
+                { table: { headerRows: 1, body: data } }
+            ],
+            styles: {
+                header: { fontSize: 18, bold: true }
+            }
+        };
+
+        pdfMake.createPdf(docDefinition).download('evacuation_list.pdf');
+
+    }
+
     $(document).ready(function() {
-        var table = $('#myTable').DataTable();
+        const table = $('#myTable').DataTable();
 
         $('#age-filter').change(function() {
-            var ageRange = $(this).val();
+            const ageRange = $(this).val();
             if (ageRange === '') {
                 table.columns(1).search('').draw();
             } else {
-                var ages = ageRange.split('-');
-                var minAge = parseInt(ages[0]);
-                var maxAge = parseInt(ages[1]);
+                const ages = ageRange.split('-');
+                const minAge = parseInt(ages[0]);
+                const maxAge = parseInt(ages[1]);
                 table.columns(1).search('^(' + minAge + '|' + (minAge+1) + '|' + (minAge+2) + '|...|' + (maxAge-2) + '|' + (maxAge-1) + '|' + maxAge + ')$', true, false).draw();
             }
         });
 
         ['gender', 'brgy', 'center', 'calamity'].forEach(function(id, index) {
             $('#' + id).on('change', function() {
-                var data = $(this).val();
+                const data = $(this).val();
                 if (id === 'gender') {
                     data = '^' + data.charAt(0);
                     table.column(3).search(data, true, false).draw();
@@ -195,10 +230,10 @@ CONCAT(bi2.lastname, ', ', bi2.firstname) as head
             });
         });
         $(function() {
-            var table = $('#myTable').DataTable();
+            const table = $('#myTable').DataTable();
 
             $('#remarks').on('change', function() {
-                var selectedValue = $(this).val();
+                const selectedValue = $(this).val();
                 table.column(4).search(selectedValue).draw();
             });
         });
